196e35218594732e654b39a3580960638af9ea1e
MathBriton <mathhbrito@gmail.com>
2025-01-31 21:47:10 -0300

    Subindo Extensions

--- Extensions/ModelStateExtension.cs
+++ Extensions/ModelStateExtension.cs
@@ -1,6 +1,16 @@
-﻿namespace WebBLOG.Extensions
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Blog.Extensions
 {
-    public class ModelStateExtension
+    public static class ModelStateExtension
     {
+        public static List<string> GetErrors(this ModelStateDictionary modelState)
+        {
+            var result = new List<string>();
+            foreach (var item in modelState.Values)
+                result.AddRange(item.Errors.Select(error => error.ErrorMessage));
+
+            return result;
+        }
     }
 }
